8e6f8fd6b28ba7c53a13468c10639d4de0a056d9
Ethan Storm <stormethan10@gmail.com>
2023-03-11 23:14:03 -0800

    added react router as a start

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,34 @@
+import './App.css'
+
 import { useState } from 'react'
 import { Container } from '@chakra-ui/react'
-import './App.css'
+
+import Home from './pages/Home'
 import Signup from './components/Signup'
+import NoPage from './pages/NoPage'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 	/*
     State
   */
 
+	// const [authenticatedUser, setAuthenticatedUser] = useState({})
+
 	return (
 		<Container centerContent maxWidth='full' className='App'>
-			<Signup />
+			<BrowserRouter>
+				{/* Header */}
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/authenticate' element={<Signup />} />
+					{/*<Route path='/about' element={<Blogs />} />*/}
+					{/*<Route path='contact' element={<Contact />} />*/}
+					<Route path='*' element={<NoPage />} />
+				</Routes>
+				{/* Footer */}
+			</BrowserRouter>
 		</Container>
 	)
 }
---
+++ client/src/pages/NoPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import { Box } from '@chakra-ui/react'
+
+export default function NoPage() {
+	return <Box>NoPage - 404 not found</Box>
+}
